b6b8ff37538ed4c2304c77f631328926eb3be4be
Bukowa <gitbukowa@gmail.com>
2024-06-15 23:51:13 +0200

    wip
    Signed-off-by: Bukowa <gitbukowa@gmail.com>

--- .changelog/has-changed.sh
+++ .changelog/has-changed.sh
@@ -8,18 +8,8 @@ if [ -n "${1-}" ]; then
   set +o allexport
 fi
 
-# Build json array of untracked and tracked files
-GIT_UNTRACKED_CHANGES=$(git ls-files --others --exclude-standard | jq -R -s -c 'split("\n")[:-1]')
-GIT_TRACKED_CHANGES=$(git diff --name-only | jq -R -s -c 'split("\n")[:-1]')
-
-# Group the untracked and tracked files
-GIT_CHANGED_FILES=$(echo "$GIT_UNTRACKED_CHANGES" "$GIT_TRACKED_CHANGES" | jq -s -c 'add | unique')
-
-# Check if the changelog file has changed
-GIT_HAS_CHANGELOG_CHANGED=$(echo $GIT_CHANGED_FILES | jq -r --arg target "$GIT_CLIFF_OUTPUT" '.[] | select(. == $target)')
-
-# Exit if no changes are detected
-if [ -z "$GIT_HAS_CHANGELOG_CHANGED" ]; then
+# Check for changes in git status output
+if ! git status --short --porcelain | cut -c 4- | grep -q "$GIT_CLIFF_OUTPUT"; then
   echo "No changes detected after generating the changelog."
   exit 1
 fi
